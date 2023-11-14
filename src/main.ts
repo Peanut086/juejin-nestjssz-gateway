219ddef4fcc17da936e7a91de51b9aa3e0961431
徐友鹏 <361618664@qq.com>
2023-11-14 21:01:50 +0800

    :tada: feat: 项目初始化

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+
+async function bootstrap() {
+  // 使用Fastify替换框架默认底层的Express
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // 接口启用版本化管理
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1', // 全局配置版本控制
+    // defaultVersion: [VERSION_NEUTRAL, '1', '2'], // 个性化版本控制，可用于控制部分接口
+  });
+
+  // 添加全局拦截器，用于格式化返回值
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
+  await app.listen(3000);
+}
+bootstrap();
