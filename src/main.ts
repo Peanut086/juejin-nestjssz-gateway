cac4fb0d8d507e6f7a1ae85e5d45ad2690bbd583
徐友鹏 <361618664@qq.com>
2023-12-12 21:28:51 +0800

    feat(飞书实体): 完成参数校验

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
+import {
+  ValidationPipe,
+  VersioningType,
+  VERSION_NEUTRAL,
+} from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
@@ -28,14 +32,17 @@ async function bootstrap() {
   // 接口启用版本化管理
   app.enableVersioning({
     type: VersioningType.URI,
-    defaultVersion: '1', // 全局配置版本控制
-    // defaultVersion: [VERSION_NEUTRAL, '1', '2'], // 个性化版本控制，可用于控制部分接口
+    // defaultVersion: '1', // 全局配置版本控制
+    defaultVersion: [VERSION_NEUTRAL], // 个性化版本控制，可用于控制部分接口
   });
 
   // 添加全局拦截器，用于格式化返回值
   app.useGlobalInterceptors(new TransformInterceptor());
   // 异常过滤器
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
+  // 启用全局字段校验
+  app.useGlobalPipes(new ValidationPipe());
 
   // 生成swagger文档
   generateDocument(app);
--- src/user/feishu/dto/FeishuMessageDto.ts
+++ src/user/feishu/dto/FeishuMessageDto.ts
@@ -1,16 +1,23 @@
 import { RECEIVE_TYPE, MSG_TYPE } from '@/helper/feishu/message';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty } from 'class-validator';
 
 export class FeishuMessageDto {
-  @ApiProperty({ example: 'email' })
+  @ApiProperty({ example: 'email', enum: RECEIVE_TYPE })
+  @IsNotEmpty()
+  @IsEnum(RECEIVE_TYPE)
   receive_id_type: RECEIVE_TYPE;
 
   @ApiProperty({ example: '361618664@qq.com' })
+  @IsNotEmpty()
   receive_id?: string;
 
   @ApiProperty({ example: '{"text":"卢本伟牛逼！"}' })
+  @IsNotEmpty()
   content?: string;
 
   @ApiProperty({ example: 'text', enum: MSG_TYPE })
+  @IsEnum(MSG_TYPE)
+  @IsNotEmpty()
   msg_type?: keyof MSG_TYPE;
 }
