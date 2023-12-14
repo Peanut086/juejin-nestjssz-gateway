c38edab2a48891ca2fa80ea25b0b1bc8b5a9d84a
徐友鹏 <361618664@qq.com>
2023-12-14 21:46:49 +0800

    feat(database): 修改实体为手:bug: 动引入，自动引入无效

--- src/common/database/database.providers.ts
+++ src/common/database/database.providers.ts
@@ -1,22 +1,21 @@
-import { DatabaseType, DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { getConfig } from '@/utils';
-import * as path from 'path';
+import { User } from '@/user/user.mysql.entity';
 
-// 设置数据库类型为mongodb
-const dataBaseType: DatabaseType = 'mysql';
-// 获取mongodb配置
 const { MYSQL_CONFIG } = getConfig();
+const dataBaseType: DataSourceOptions['type'] = MYSQL_CONFIG.type;
+
+// 获取mongodb配置
 const MYSQL_DATABASE_CONFIG = {
   ...MYSQL_CONFIG,
   type: dataBaseType,
-  entities: [
-    path.join(
-      __dirname,
-      `dist/../**/*.${MYSQL_CONFIG.entities}.entity{.ts,.js}`,
-    ),
-  ],
+  entities: [User],
 };
-const MYSQL_DATA_SOURCE = new DataSource(MYSQL_DATABASE_CONFIG);
+
+const MYSQL_DATA_SOURCE = new DataSource({
+  ...MYSQL_DATABASE_CONFIG,
+});
+
 // 数据库实例注入
 export const DatabaseProviders = [
   {
@@ -25,10 +24,10 @@ export const DatabaseProviders = [
       try {
         if (!MYSQL_DATA_SOURCE.isInitialized)
           await MYSQL_DATA_SOURCE.initialize();
+        console.log('数据库初始化成功！');
         return MYSQL_DATA_SOURCE;
       } catch (err) {
-        console.log('Peanut console...😏😣😆😁🤣😂\n', '数据库连接错误！\n');
-        console.log('Peanut console...😏😣😆😁🤣😂\n', err + '\n');
+        console.log('数据库初始化失败！' + err.message);
       }
     },
   },
